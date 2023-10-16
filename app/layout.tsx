03eea702e32cad07cfe80232ad53d967f0b09526
HoangLeCoder <hle070302@gmail.com>
2023-10-16 23:41:12 +0700

    Add @nextui-org lib and add dropdown to nav menu responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import * as React from "react";
+
+import { NextUIProvider } from "@nextui-org/react";
 
 export const metadata: Metadata = {
     title: "Harry Travel",
@@ -15,12 +18,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body>
-                <Navbar />
-                <main className="relative overflow-hidden">{children}</main>
-                <Footer />
-            </body>
-        </html>
+        // <NextUIProvider>
+            <html lang="en">
+                <body>
+                    <Navbar />
+                    <main className="relative overflow-hidden">{children}</main>
+                    <Footer />
+                </body>
+            </html>
+        // </NextUIProvider>
     );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import {
+    Dropdown,
+    DropdownTrigger,
+    DropdownMenu,
+    DropdownItem,
+} from "@nextui-org/dropdown";
 
 const Navbar = () => {
     return (
@@ -28,20 +36,43 @@ const Navbar = () => {
 
             <div className="lg:flexCenter hidden">
                 <Button
-                  type = "button"
-                  title = "Login"
-                  icon= "/user.svg"
-                  variant = "btn_dark_green"
+                    type="button"
+                    title="Login"
+                    icon="/user.svg"
+                    variant="btn_dark_green"
                 />
             </div>
 
-            <Image
-              src="/menu.svg"
-              alt="menu"
-              width={32}
-              height={32}
-              className="inline-block cursor-pointer lg:hidden"
-            />
+            <Dropdown>
+                <DropdownTrigger>
+                    <Image
+                        src="/menu.svg"
+                        alt="menu"
+                        width={32}
+                        height={32}
+                        className="inline-block cursor-pointer lg:hidden"
+                    />
+                </DropdownTrigger>
+                <DropdownMenu aria-label="Static Actions">
+                    {NAV_LINKS.map((link) => (
+                        <DropdownItem key={link.key}>
+                            <Link
+                                href={link.href}
+                                className="regular-16 text-gray-50 flexCenter cursor-pointer py-2.5"
+                            >
+                                {link.label}
+                            </Link>
+                        </DropdownItem>
+                        // <Link
+                        //     href={link.href}
+                        //     key={link.key}
+                        //     className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                        // >
+                        //     {link.label}
+                        // </Link>
+                    ))}
+                </DropdownMenu>
+            </Dropdown>
         </nav>
     );
 };
