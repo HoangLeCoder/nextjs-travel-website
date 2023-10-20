9068cc443181b91140d56f7e364d5fc05587bf31
HoangLeCoder <hle070302@gmail.com>
2023-10-20 16:31:47 +0700

    Complete GetApp session

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,20 +1,25 @@
-import  Image  from "next/image";
+import Image from "next/image";
 
 type ButtonProps = {
-    type: 'button' | 'submit';
+    type: "button" | "submit";
     title: string;
     icon?: string;
     variant: string;
+    full?: boolean;
+};
 
-}
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+    return (
+        <button
+            type={type}
+            className={`flexCenter gap-3 rounded-full border ${variant} ${
+                full && "w-full"
+            }`}
+        >
+            {icon && <Image src={icon} alt={title} width={24} height={24} />}
+            <label className="bold-16 whitespace-pre cursor-pointer">{title}</label>
+        </button>
+    );
+};
 
-const Button = ({type, title, icon, variant}: ButtonProps) => {
-  return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
-        {icon && <Image src={icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-pre">{title}</label>
-    </button>
-  )
-}
-
-export default Button
+export default Button;
